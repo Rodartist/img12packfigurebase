fd7543d0ee16efe40ea46aa69db26454be186131
Rodartist <git@github.com:Rodartist/C64Music.git>
2023-06-14 16:11:35 -0500

    3.7k git push of newxtermstartshift approximately part six of ten

---
+++ newxtermstartshift/6991+6991-1cellalt.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/expect -f
+
+# Get a Bash shell
+spawn -noecho bash
+
+# Wait for a prompt
+expect "$ "
+
+# Type something
+send "sh /home/molasses/tmuxsync/simplechangestogtkmsui-5-12-2023/gtkmsui/SIDgtk/xterms.sh setxtermtitle\n xdotool keydown Super_L key t keyup Super_L keydown Alt_L key r keyup Alt_L\ncd /home/molasses/tmuxsync/simplechangestogtkmsui-5-12-2023/gtkmsui/SIDgtk/SID/C64Music/ \nsidplayfp MUSICIANS/B/Bod/Coprolalia.sid \n echo hldny &&"
+
+#Shell command: cat thrulist1awk.txt | awk -F '-' {'print $1"/"$2"/"$3'}
+#https://rr-project.org/
+# ~/tmuxsync/simplechangestogtkmsui-5-12-2023/gtkmsui/SIDgtk/SID/C64Music/
+# Hand over control to the user
+interact
+
+exit
